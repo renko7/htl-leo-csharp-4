981dede67b2614d3ed76931df593746cc80afc4e
renko7 <66572823+renko7@users.noreply.github.com>
2022-09-22 20:37:47 -0400

    installing EF nuget packages and creating facts model

---
+++ exercises/9080-ef-chuck-norris/ChuckNorrisClassLibrary/Models/ChuckNorrisFact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChuckNorrisClassLibrary
+{
+    public class ChuckNorrisFact
+    {
+        public int Id { get; set; }
+
+        [MaxLength(40)]
+        public String ChuckNorrisId { get; set; } = String.Empty;
+
+        [MaxLength(1024)]
+        public String Url { get; set; } = String.Empty;
+
+        public String Joke { get; set; } = String.Empty;
+    }
+}
